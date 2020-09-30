1744663c404861cd83a02f43fa61701cdf794481
Sigi Jaeckel <sigij55@gmail.com>
2020-09-30 13:14:39 -0400

    completed exp_seq

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,16 @@
 # Your code here
+cache = {}
 
 
 def expensive_seq(x, y, z):
     # Your code here
+    if x <= 0:
+        return y + z
 
+    if (x, y, z) not in cache:
+        cache[x, y, z] = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
+        # return cache[x, y, z]
+    return cache[x, y, z]
 
 
 if __name__ == "__main__":
@@ -12,3 +19,5 @@ if __name__ == "__main__":
         print(f"{i*2} {i*3} {i*4} = {x}")
 
     print(expensive_seq(150, 400, 800))
+
+
