2af16bf2c3912fb559726f72d2c28db0bc49b3d0
Sigi Jaeckel <sigij55@gmail.com>
2020-10-01 01:50:47 -0400

    completed word_count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,22 @@
 def word_count(s):
     # Your code here
+    my_string = s.lower().split()
+    print(my_string)
+    dict = {}
+    ignore = {',','"',':',';','.', '-', '+', '=', '/', '\\' , '|', '[', ']' , '{', '}','(', ')', '*', '^', '&'}
+    for word in my_string:
+        # word = raw_word.strip(ignore)
+        # word = "".join(c for c in word if c.isalpha())
+        new_word = "".join(c for c in word if c not in ignore)
+        if len(new_word) == 0:
+            return dict
+        print(new_word)
+        if new_word not in dict:
+            dict[new_word] = 0
 
+        dict[new_word] += 1
+    print(dict)
+    return dict
 
 
 if __name__ == "__main__":
